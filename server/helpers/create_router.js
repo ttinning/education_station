d9b68603f2cd5c5d910789a1acb99bb1c324e990
Tristan Tinning <tristan_tinning@hotmail.com>
2021-07-03 11:16:40 +0100

    delete router created

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -42,4 +42,21 @@ const createRouter = function(collection) {
             res.json({ status: 500, error: err })
         });
     });
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .deleteOne(
+            { _id: ObjectID(id) }
+        )
+        .then((result) => {
+            res.json(result)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err })
+        });
+    });
+
 }
