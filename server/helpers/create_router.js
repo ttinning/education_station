c9e063df244f9f73de636bd5d0953bab40de07c3
Tristan Tinning <tristan_tinning@hotmail.com>
2021-07-03 11:23:30 +0100

    put method added

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -59,4 +59,23 @@ const createRouter = function(collection) {
         });
     });
 
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection
+        .updateOne(
+            { _id: ObjectID(id) },
+            { $set: updatedData }
+        )
+        .then((result) => {
+            res.json(result)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err })
+        });
+    });
 }
+
+module.exports = createRouter
