5eebea1661db7d2434f40960da5bec47ca461a05
OhmniD <dave@ohmnid.com>
2021-07-03 18:58:47 +0100

    further styling of navbar - add logo, font

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,9 @@ import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav id="navbar">
-            <ul>
+        <nav>
+            <h1>Education App</h1>
+            <ul id="navbar">
                 <Link to="/">Home </Link>
             </ul>
         </nav>
