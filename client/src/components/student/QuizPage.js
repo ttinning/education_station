4032cffd81be1e3d1b064a0693321fe6f986b701
Gary Darnell <gdarnell9@gmail.com>
2021-07-03 17:59:58 +0100

    Displays words for topic on quizpage.

--- client/src/components/student/QuizPage.js
+++ client/src/components/student/QuizPage.js
@@ -3,11 +3,12 @@ import { useLocation } from "react-router-dom"
 const QuizPage = () => {
 
     const data = useLocation()
+    const topic = data.state.topic
     console.log(data.state.topic)
 
     return(
         <div>
-            {/* <p>{topic.word_list}</p> */}
+            <p>{topic.word_list}</p>
             <p>This is the Quizpage</p>
         </div>
         
