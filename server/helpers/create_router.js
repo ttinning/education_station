fc450f006537f0b01ee34f081e91f1d8b75fffe2
Tristan Tinning <tristan_tinning@hotmail.com>
2021-07-03 11:08:03 +0100

    show index router created

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -13,7 +13,20 @@ const createRouter = function(collection) {
         .catch((err) => {
             console.error(err);
             res.status(500);
-            res.json({ status: 500, error: err })
+            res.json({ status: 500, error: err });
         });
     });
+
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .findOne({ _id: ObjectID(id) })
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+        });
+    });
+    
 }
