440c23549cdc97204c39824264e06a808db7b43e
Zaki Yusaf <zakiyusaf@hotmail.co.uk>
2021-07-03 11:09:22 +0100

    Setup the server.js file but have not connected to a mongo database

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+// const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('helpers/create_router.js');
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors());
+
+// MongoClient.connect('mongodb://localhost:27017'.then(client) => {
+    
+// })
+
+app.listen(5000, function() {
+    console.log(`Listening on port ${ this.address().port}`);
+})
