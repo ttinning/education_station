028904e9a6cc2b257eb78a3a27505fc8141e46c9
sara <smhumphries@hotmail.co.uk>
2021-07-04 12:03:46 +0100

    account details prop in the word list

--- client/src/components/parent/Word.js
+++ client/src/components/parent/Word.js
@@ -1,6 +1,23 @@
 
 
-const Word = ({word}) => {
+const Word = ({word, accounts}) => {
+
+   
+    
+   
+    const completedWords = accounts[0].student.learning_status.completed_topics[0].words_first_time;
+    console.log(completedWords);
+
+    const CompletedStatus = completedWords.some((completedWord) =>  {
+        return word === completedWord;
+    });
+
+    // const completedStatus = accounts.student.learning_status.completed_topics.words_first_time.some((completed_word) => {
+    //     return completed_word === word;
+    // });
+
+    // completedStatus();
+
     return (
         <li>{word}</li>
     )
