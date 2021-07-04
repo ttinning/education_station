dd4b833636575d22e0023fd25d63013dff3e0603
sara <smhumphries@hotmail.co.uk>
2021-07-04 12:42:27 +0100

    shows which words were answered correctly ffirst time on parent dashboard

--- client/src/components/parent/Word.js
+++ client/src/components/parent/Word.js
@@ -2,24 +2,25 @@
 
 const Word = ({word, accounts}) => {
 
-   
-    
-   
     const completedWords = accounts[0].student.learning_status.completed_topics[0].words_first_time;
+   
     console.log(completedWords);
+    const calculateCompletedStatus = function() {
+        
+        for (let completedWord of completedWords) {
+            if (word === completedWord) {
+                return "correct first time!";
+            };
+            
+        };
+    };
+    
 
-    const CompletedStatus = completedWords.some((completedWord) =>  {
-        return word === completedWord;
-    });
+    const completedStatus = calculateCompletedStatus();
 
-    // const completedStatus = accounts.student.learning_status.completed_topics.words_first_time.some((completed_word) => {
-    //     return completed_word === word;
-    // });
-
-    // completedStatus();
 
     return (
-        <li>{word}</li>
+        <li>{word} {completedStatus}</li>
     )
 };
 
