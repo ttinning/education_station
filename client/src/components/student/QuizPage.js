3c39bb6479473890cf1432ec7e0a7bd054825a2b
Dave Murray <dave@ohmnid.com>
2021-07-05 09:27:05 +0100

    add Gary's bug fix - jumbled word only appears once

--- client/src/components/student/QuizPage.js
+++ client/src/components/student/QuizPage.js
@@ -8,15 +8,17 @@ const QuizPage = () => {
     const topic = data.state.topic
 
     const [questionNumber, setQuestionNumber] = useState(0);
-    const [answer, setAnswer] = useState("");
+    const [answer, setAnswer] = useState('');
     const [wordInfo, setWordInfo] = useState({});
     const [showHint, setShowHint]= useState(false);
     const [showAnswer, setShowAnswer]= useState(false);
     const [answerCorrect, setAnswerCorrect] = useState(false);
+    const [randomWord, setRandomWord] = useState('')
 
     useEffect(() => {
         WordService.getWordInfo(topic.word_list[questionNumber])
             .then(res => setWordInfo(res))
+            .then(letterRandomise(quizWord));
     }, [questionNumber]);
 
     useEffect(() => {
@@ -52,10 +54,8 @@ const QuizPage = () => {
         while (quizWord.length > 0) {
             shuffleWord += quizWord.splice(quizWord.length * Math.random() << 0,1);
         }
-        return shuffleWord;
+        setRandomWord(shuffleWord);
     }
-
-    const randomWord = letterRandomise(quizWord)
 
     const checkAnswer = (event) => {
         setAnswer(event.target.value.toLowerCase())
@@ -68,7 +68,7 @@ const QuizPage = () => {
             {Object.keys(wordInfo).length > 0 ? <img src={wordInfo.definitions[0].image_url } alt={wordInfo.word}></img> : null}
             
             <button onClick={handleHintClick}>Show Hint</button>
-            {showHint ? <p>{letterRandomise(quizWord)}</p> : null}
+            {showHint ? <p>{randomWord}</p> : null}
 
             <button onClick={handleRevealClick}>Reveal answer</button>
             {showAnswer ? <p>The answer is {wordInfo.word}</p> : null}
