a7d5efcb125895124bed1530e4b0db4b43df5571
sara <smhumphries@hotmail.co.uk>
2021-07-06 17:42:50 +0100

    adds update to accounts trophies when adding a new topic

--- client/src/components/parent/TopicForm.js
+++ client/src/components/parent/TopicForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TopicService from "../../services/TopicService";
 import WordService from "../../services/WordService";
+import AccountsService from "../../services/AccountsService";
 
 const TopicForm = ({addNewTopic, accounts}) => {
 
@@ -14,31 +15,40 @@ const TopicForm = ({addNewTopic, accounts}) => {
 
     const handleFormSubmit = (evt) => {
         evt.preventDefault()
+
+        const formattedData = formatNewTopic();
+        TopicService.postTopic(formattedData)
+            .then((data) => {addNewTopic(formattedData)})
+
+        updateTrophiesOnAccount();
+        resetForm();
+        
+    };
+
+    const updateTrophiesOnAccount = () => {
+        const temp = {...accounts[0]}
+        temp.student.topics_trophies[`${formData.title}`] = {"quiz": false, "drag": false, "audio": false};
+        delete temp._id
+        console.log(temp);
+        AccountsService.updateAccounts(accounts[0]._id, temp)
+    };
+
+    const formatNewTopic = () => {
         let words = Object.values(formData);
         words.splice(0, 1);
-        
         let formattedData = {
             "title": formData.title,
             "word_list": words
         };
-        console.log(formattedData);
-        
-        TopicService.postTopic(formattedData)
-            .then((data) => {addNewTopic(formattedData)})
-
-        const form = document.querySelector('#new-topic-form')
-        form.reset();
-        setFormData({});
+        return formattedData;
         
     };
 
-    // const updateTrophiesOnAccount = () => {
-    //     const temp = {...accounts[0]}
-    //     newTrophiesObject = {};
-    //     temp.student.topics_trophies
-    //     delete temp._id
-    //     AccountsService.updateAccounts(accounts[0]._id, temp)
-    // };
+    const resetForm = () => {
+        const form = document.querySelector('#new-topic-form')
+        form.reset();
+        setFormData({});
+    };
 
     return (
         <div>
