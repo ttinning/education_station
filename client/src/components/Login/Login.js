9801c7554d34acb096ff0f62fd6a4514f8656f1c
Gary Darnell <gdarnell9@gmail.com>
2021-07-06 19:52:33 +0100

    Basic login completed with token getting passed. Needs to re-login for every hard refresh.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,17 +1,43 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
 import './Login.css'
 
-const Login = () => {
+async function loginUser(credentials) {
+    return fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+}
+const Login = ({ setToken }) => {
+
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('') 
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        const token = await loginUser({
+            username,
+            password
+        })
+        setToken(token)
+    }
+
     return(
         <div className='login-wrapper'>
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" name="username" id="username"/>
+                    <input type="text" name="username" id="username" onChange={event => setUserName(event.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" name="password" id="password"/>
+                    <input type="password" name="password" id="password" onChange={ event => setPassword(event.target.value)}/>
                 </label>
                 <div>
                     <button type="submit">Submit</button>
@@ -19,6 +45,10 @@ const Login = () => {
             </form>
         </div>
     )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
 
 export default Login;
